67b4eb3e056a457ce8e7c95ef4e98c3a3dc10396
hasegawa-101 <hasegawa@101de-sign.com>
2022-06-23 23:23:49 +0900

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let mut x = 5;
+    println!("The value of x is: {}", x); // xの値は{}です
+    x = 6;
+    println!("The value of x is: {}", x);
+
+    let spaces = "   ";
+    let spaces = spaces.len();
+}
+
+const MAX_POINTS: u32 = 100_000;
